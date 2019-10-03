bfd289cbc57f451ec9041b00fd22c5b2c0dce250
Joel Stockamp <stockampj@gmail.com>
2019-10-03 10:49:39 -0700

    add more testing structure

---
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrabble.Models;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class SrabbleCalc
+  {
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    }
+  }
+}
